da18742b99fcb067ba9c5a1d9a8f98f9a97a4cc9
Ismael CasLop <mmcready07@gmail.com>
2020-06-28 20:59:25 -0500

    Updates

---
+++ precache-manifest.eae713bf6a8a08763eec0f81fd074239.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436c1e27d268e6be80bd91adba499152",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "71c477ddaf9a6b342fb4",
+    "url": "/pokemon-app/static/css/main.40740826.chunk.css"
+  },
+  {
+    "revision": "69098d166f0336b3f041",
+    "url": "/pokemon-app/static/js/2.9cf03519.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pokemon-app/static/js/2.9cf03519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c477ddaf9a6b342fb4",
+    "url": "/pokemon-app/static/js/main.c727c662.chunk.js"
+  },
+  {
+    "revision": "53b9c226502149a401b8",
+    "url": "/pokemon-app/static/js/runtime-main.9ee58c44.js"
+  },
+  {
+    "revision": "988dee805f702c7ea61b023537268ad6",
+    "url": "/pokemon-app/static/media/logo.988dee80.png"
+  }
+]);
